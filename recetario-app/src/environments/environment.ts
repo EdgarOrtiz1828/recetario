5a8107ac1b706700819ac917b978cc87596aa560
EdgarOrtiz1828 <124111298+EdgarOrtiz1828@users.noreply.github.com>
2023-05-29 16:47:55 -0600

    prueba 1
    faltan los botones de editar, borrar y agregar receta

---
+++ recetario-app/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+   firebaseConfig: {
+    apiKey: "AIzaSyCWg0JnYH0K4BMAHy-pUwduypQE-gpOBQE",
+    authDomain: "lab-031-eeoc.firebaseapp.com",
+    projectId: "lab-031-eeoc",
+    storageBucket: "lab-031-eeoc.appspot.com",
+    messagingSenderId: "911507145752",
+    appId: "1:911507145752:web:cc8d766a37675eb14b2f6d"
+  },
+  apiKeyMaps: 'AIzaSyAPEAXSoGmOdkMvI-DL7RvO1vID7IKo0xk'   
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
